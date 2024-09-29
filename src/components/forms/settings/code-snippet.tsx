4430e9c575ff23bde167e907d92e6d2a5d274e94
ajay-dv8 <homesevice300@gmail.com>
2024-09-29 08:12:22 +0000

    prod commit test 5

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -26,16 +26,16 @@ const CodeSnippet = ({ id }: Props) => {
         }
     \`)
     
-    iframe.src = "http://localhost:3000/chatbot"
+    iframe.src = "https://divesai.vercel.app/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-        if(e.origin !== "http://localhost:3000") return null
+        if(e.origin !== "https://divesai.vercel.app/") return null
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
+        iframe.contentWindow.postMessage("${id}", "https://divesai.vercel.app/")
     })
   `
 
